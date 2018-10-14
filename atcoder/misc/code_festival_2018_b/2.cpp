9875737bcc8846cadf3a9398584dd5b97233022a
tMasaaa <noumodulity@gmail.com>
2018-10-14 23:51:45 +0900

    [add] code_b

---
+++ atcoder/misc/code_festival_2018_b/2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+//---------------------------
+using namespace std;
+//---------------------------
+#define REP(i,n) for(int i = 0; i < (n); i++)
+#define P(x) cout << (x) << "\n"
+#define fcout cout << fixed << setprecision(18)
+
+#define MOD 1000000007 // 1e9+7
+#define PI 3.1415926535
+/*
+3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+*/
+#define ll long long int // 10^18
+#define INF 1000000001 // 1e9+1
+#define LINF 1000000000000000001 // 1e18+1
+
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+//---------------------------
+int N,X,ind,sm=0;
+int a[110],b[110];
+int mx=-1;
+int main(){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+
+  // ifstream in("input.txt");
+  // cin.rdbuf(in.rdbuf());
+
+  cin>>N>>X;
+  REP(i,N){
+    cin>>a[i]>>b[i];
+    if(mx<b[i]){
+      mx=b[i];
+      ind=i;
+    }
+    sm+=a[i]*b[i];
+  }
+  sm+=X*b[ind];
+  P(sm);
+
+  return 0;
+}
